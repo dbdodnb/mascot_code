7d78cba4139709dd37d8e57051492ad72d9274b6
Danyil Mamentovych <dmamentovych@kse.org.ua>
2024-10-24 16:35:08 +0300

    auth

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+    return(
+        <div className="header">
+            <div className="nav"></div>
+            <div className="header__info"></div>
+        </div>
+    )
+}
+
+export default Header
