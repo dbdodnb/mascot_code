36b21ef740886c8f7e8d1518a146dc3d56f291ac
Danyil Mamentovych <dmamentovych@kse.org.ua>
2024-10-25 11:32:59 +0300

    nav-start

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,17 @@ import React from "react";
 function Header() {
     return(
         <div className="header">
-            <div className="nav"></div>
-            <div className="header__info"></div>
+            <div className="nav padding">
+                <div className="nav__logo"> </div>
+                <div className="nav__info">
+                    <ul>
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Accomodations</a></li>
+                        <li><a href="#">FAQ</a></li>
+                    </ul>
+                </div>
+            </div>
+            <div className="header__info padding"></div>
         </div>
     )
 }
