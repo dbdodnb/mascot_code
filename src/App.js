08ff1a8dbddfbfcd7b1ee94f41d54e1656fa79a2
Danyil Mamentovych <dmamentovych@kse.org.ua>
2024-11-07 18:08:53 +0200

    participants fix

--- src/App.js
+++ src/App.js
@@ -11,10 +11,20 @@ import Feedback from './components/Feedback';
 import Partners from './components/Partners';
 import Faq from './components/Faq';
 import Footer from './components/Footer';
+import { Helmet } from 'react-helmet';
+import metaPhoto from '../src/img/header/1.jpg'
 
 function App() {
   return (
     <>
+      <Helmet>
+        <title>MASCOT 2010-2025 | Summer Camp of Teamwork</title>
+        <meta
+          name="description"
+          content="Camp MASCOT is a 10-day program for Ukrainian proactive teenagers aimed to build a bright future in Ukraine."
+        />
+        <meta property="og:image" content={metaPhoto} />
+      </Helmet>
       <Header />
       <MASCOT />
       <About />
