39b1588fad7a8fbab327ebfa1bcae1815da179ec
Danyil Mamentovych <dmamentovych@kse.org.ua>
2024-12-06 14:00:52 +0200

    final(no link)

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -2,12 +2,17 @@ import React, { useState, useEffect } from "react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import { useTranslation } from 'react-i18next';
-import placeholder from '../img/placeholder.png';
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/autoplay";
 import { Autoplay, Pagination } from "swiper/modules";
+import respect from '../img/values/повага.png'
+import inclusivity from '../img/values/інклюзивність.png'
+import communication from '../img/values/комунікації.png'
+import trust from '../img/values/довіра.png'
+import community from '../img/values/спільнота.png'
+import equality from '../img/values/рівність.png'
 
 function Values() {
     const { t, i18n } = useTranslation();
@@ -27,12 +32,12 @@ function Values() {
     }, []);
 
     const blocks = [
-        { title: t('respect'), info: t('respectDef'), image: placeholder },
-        { title: t('inclusivity'), info: t('inclusivityDef'), image: placeholder },
-        { title: t('communication'), info: t('communicationDef'), image: placeholder },
-        { title: t('trust'), info: t('trustDef'), image: placeholder },
-        { title: t('community'), info: t('communityDef'), image: placeholder },
-        { title: t('equality'), info: t('equalityDef'), image: placeholder },
+        { title: t('respect'), info: t('respectDef'), image: respect },
+        { title: t('inclusivity'), info: t('inclusivityDef'), image: inclusivity },
+        { title: t('communication'), info: t('communicationDef'), image: communication },
+        { title: t('trust'), info: t('trustDef'), image: trust },
+        { title: t('community'), info: t('communityDef'), image: community },
+        { title: t('equality'), info: t('equalityDef'), image: equality },
     ];
 
     const toggleExpand = (index) => {
@@ -77,10 +82,11 @@ function Values() {
                                             style={{
                                                 display: isExpanded ? "none" : "block",
                                                 backgroundImage: `url(${block.image})`,
-                                                backgroundSize: "cover",
+                                                backgroundSize: 'contain',
+                                                backgroundRepeat: 'no-repeat',
                                                 backgroundPosition: "center",
-                                                width: "200px",
-                                                height: "200px",
+                                                width: "100%",
+                                                height: "auto",
                                                 border: "none",
                                                 marginTop: "0px",
                                             }}
@@ -123,8 +129,9 @@ function Values() {
                                         style={{
                                             display: isExpanded ? "none" : "block",
                                             backgroundImage: `url(${block.image})`,
-                                            backgroundSize: "cover",
+                                            backgroundSize: "contain",
                                             backgroundPosition: "center",
+                                            backgroundRepeat: 'no-repeat',
                                             width: "200px",
                                             height: "200px",
                                             border: "none",
